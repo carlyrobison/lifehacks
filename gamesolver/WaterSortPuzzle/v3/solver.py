b37280dd1f1d70618396cd75d59fbdb8ce9df6f0
Carly Robison <carlyellenrobison@gmail.com>
2023-01-03 00:01:48 -0600

    Efficiently-ish solves a much harder problem.

--- gamesolver/WaterSortPuzzle/v3/solver.py
+++ gamesolver/WaterSortPuzzle/v3/solver.py
@@ -16,28 +16,34 @@ def solve(game: GameState, explored_gamestates : List[GameState] = []) -> bool:
     
     # Base case 2: Loops :(
     if game in explored_gamestates:
-        print('Found a loop')
+        # print('Found a loop')
         return False  # Unsolvable
     
     explored_gamestates.append(game)
 
     numVials = len(game.vials_)
     for i, j in product(range(numVials), range(numVials)):
-        if game.isValidMove((i, j)):
-            print("{2}Moving vial {0} into vial {1}".format(i, j, len(explored_gamestates)* '\t'))
+        if game.isValidMove((i, j)):      
             newgame = copy.deepcopy(game)
             newgame.makeMove((i, j))
             if solve(newgame, explored_gamestates):  # This returning True means there is a valid solve path.
                 print('------------')
+                print("{2} Moved vial {0} into vial {1}".format(i, j, len(explored_gamestates)* '\t'))
                 print(game)
                 return True
     
     explored_gamestates.pop()
     # Base case 3: No valid moves, or all moves end in failure
-    print('No winning moves :(')
+    # print('No winning moves :(')
     return False
 
 init_string: str = 'aabbbbaa'
 g = GameState(init_string)
 print(g)
-solve(g)
+assert(solve(g))
+
+# TODO: Fix move selection so it doesn't move the green back and forth
+init_string: str = 'otbwlagpwtlvvaaykgrltproboprlwgvbvybwogakykprkyt'
+g = GameState(init_string)
+print(g)
+assert(solve(g))
