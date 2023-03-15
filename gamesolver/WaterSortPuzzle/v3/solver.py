399d543f4e785b4824b68a9337758b31842400ab
Carly Robison <carlyellenrobison@gmail.com>
2023-03-14 19:16:22 -0500

    Add debug option and timeit for profiling.

--- gamesolver/WaterSortPuzzle/v3/solver.py
+++ gamesolver/WaterSortPuzzle/v3/solver.py
@@ -2,8 +2,10 @@ import copy
 from itertools import product
 from typing import List
 from game_state_model import GameState
+import timeit
 
 MAX_MOVES = 50
+DEBUG = False
 
 # TODO optimize with hashing
 def solve(game: GameState, explored_gamestates : List[GameState] = []) -> bool:
@@ -12,12 +14,14 @@ def solve(game: GameState, explored_gamestates : List[GameState] = []) -> bool:
         print('Solved! Printing in reverse order....')
         print('------------')
         print("Metric: {}".format(game.metricValue()))
-        # print(game)
+        if (DEBUG):
+            print(game)
         return True
     
     # Base case 2: Loops :(
     if game in explored_gamestates:
-        # print('Found a loop')
+        if (DEBUG):
+            print('Found a loop')
         return False  # Unsolvable
     
     explored_gamestates.append(game)
@@ -28,31 +32,34 @@ def solve(game: GameState, explored_gamestates : List[GameState] = []) -> bool:
             newgame = copy.deepcopy(game)
             newgame.makeMove((i, j))
             if newgame.metricValue() > game.metricValue() and solve(newgame, explored_gamestates):  # This returning True means there is a valid solve path.
-                print('------------')
-                print("{2} Moved vial {0} into vial {1}".format(i, j, len(explored_gamestates)* '\t'))
-                print("Metric: {}".format(game.metricValue()))
-                # print(game)
+                print("Metric: {3} {2}> Moved vial {0} into vial {1}".format(i, j, 5 * '-', game.metricValue()))
+                if (DEBUG):
+                    print(game)
                 return True
     
     explored_gamestates.pop()
     # Base case 3: No valid moves, or all moves end in failure
-    # print('No winning moves :(')
+    if (DEBUG):
+        print('No winning moves :(')
     return False
 
 # Simple, but not trivial
 init_string: str = 'aabb bbaa'
 g = GameState(init_string)
 print(g)
+# print(timeit.timeit(lambda: solve(g), number=100))
 assert(solve(g))
 
 # Typical game
 init_string: str = 'otbw lagp wtlv vaay kgrl tpro bopr lwgv bvyb woga kykp rkyt'
 g = GameState(init_string)
 print(g)
-assert(solve(g))
+# assert(solve(g))
+print(timeit.timeit(lambda: solve(g), number=10))
 
-# Another game
-init_string: str = 'yoro wyaa pbyl lgkw wkak vrgv tvgl pblo gbtk rpbt wovp trya'
-g = GameState(init_string)
-print(g)
-assert(solve(g))
+
+# # Another game
+# init_string: str = 'yoro wyaa pbyl lgkw wkak vrgv tvgl pblo gbtk rpbt wovp trya'
+# g = GameState(init_string)
+# print(g)
+# assert(solve(g))
