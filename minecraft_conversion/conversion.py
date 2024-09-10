e0601c5c2ac14ae943c3732d51acc0a3fa9c39dc
Carly Robison <carly.ellen.robison@gmail.com>
2024-09-09 23:24:27 -0500

    Update script

--- minecraft_conversion/conversion.py
+++ minecraft_conversion/conversion.py
@@ -1,15 +1,23 @@
 import pandas as pd
-import imageio
+import imageio.v2 as imageio
 import numpy as np
 
-img_location = "REDACTED.bmp"
+img_location = "~/Documents/minecraft/worldmapflattened3.bmp"
 
 img = (imageio.imread(img_location, pilmode = 'RGB'))
 data = np.array(img)
+unique_colors = []
 
 for i in data:
-    print(i)
+    for j in i:
+        # print(j)
+        if tuple(j.tolist()) not in unique_colors:
+            unique_colors.append(tuple(j.tolist()))
+print(len(unique_colors))
+unique_colors.sort()
+for c in unique_colors:
+    print(c)
 
-pd.DataFrame(data[:,:,0]).to_csv("image0R.csv")
-pd.DataFrame(data[:,:,1]).to_csv("image0G.csv")
-pd.DataFrame(data[:,:,2]).to_csv("image0B.csv")
+pd.DataFrame(data[:,:,0]).to_csv("image3R.csv")
+pd.DataFrame(data[:,:,1]).to_csv("image3G.csv")
+pd.DataFrame(data[:,:,2]).to_csv("image3B.csv")
