cc6aadc59ea9c168f1598b6a4fbd92e33618f7d1
Carly Robison <carly.ellen.robison@gmail.com>
2024-10-16 00:17:26 -0500

    Bit of scrolling, bit of positioning

--- grandcanyon/js/main.js
+++ grandcanyon/js/main.js
@@ -1,13 +1,27 @@
 const riverMiles = 220;
-const pxPerMile = 50 
+const pxPerMile = 50;
+const padding = 20;  // pixels
+var headerSize = 0;  // pixels
 
 function screenPos(riverMile) {
-    return riverMile * pxPerMile
+    riverScreenLength = riverMiles * pxPerMile;
+    return headerSize + padding + riverScreenLength - (riverMile * pxPerMile)
 }
 
 $(document).ready(function(){
     // jQuery methods go here...
+    headerSize = $(".header").height()
 
-    $("#rivermap").height(screenPos(riverMiles))
+    $("#rivermap").height(padding + (riverMiles * pxPerMile) + padding)
+
+    // TODO: add a bunch of thingies
+    // <!-- Consider using popovers instead of badges.-->
+    // <h3><span class="badge bg-secondary">POI</span> Canyon --></h3>
+    // Why is it not showing badges?
+
+    $(".left-bank").append(
+        $("<h5></h5>").append($("<span></span>").text("POI").addClass("badge").addClass("bg-secondary"))
+        .text("Canyon")
+        .css("transform", `translateY(${screenPos(100)}px)`))
   
   });
