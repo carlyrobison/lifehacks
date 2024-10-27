4e53a01c231d1dda85f9d792ebb3cb47fa52e4fc
Carly Robison <carly.ellen.robison@gmail.com>
2024-10-26 20:14:21 -0500

    Added campsites
    Added campsites with partial data; added first drafts of other CSVs

--- grandcanyon/js/main.js
+++ grandcanyon/js/main.js
@@ -1,5 +1,5 @@
-const riverMiles = 220;
-const pxPerMile = 50;
+const riverMiles = 300;
+const pxPerMile = 100;
 const padding = 20;  // pixels
 var headerSize = 0;  // pixels
 
@@ -8,10 +8,28 @@ function screenPos(riverMile) {
     return headerSize + padding + riverScreenLength - (riverMile * pxPerMile)
 }
 
-// TODO: add a bunch of items here.
 function writeCampsites(){
-
+    $.get("data/Grand Canyon POIs - Camps.csv", function(csvdata) {
+        // csv is populated with the file contents
+        var data = $.csv.toObjects(csvdata);
+        for (const camp of data) {
+            // Consider using popovers instead of badges.
+            if (camp['Side'] == 'L') {
+                $(".left-bank").append(
+                    $("<p></p>").html('<span class="badge bg-primary">Camp</span> ' + camp['Name'] + ' -->')
+                    .css("transform", `translateY(${screenPos(parseFloat(camp['River Mile']))}px)`))
+            } else if (camp['Side'] == 'R') {
+                $(".right-bank").append(
+                    $("<p></p>").html('<-- ' + camp['Name'] + ' <span class="badge bg-primary">Camp</span>')
+                    .css("transform", `translateY(${screenPos(parseFloat(camp['River Mile']))}px)`))
+            } else {
+                console.log('Improper side for camp:' + camp)
+            }
+        }
+    });
 }
+
+
 
 $(document).ready(function(){
     // jQuery methods go here...
@@ -23,5 +41,7 @@ $(document).ready(function(){
     $(".left-bank").append(
         $("<h5></h5>").html('<span class="badge bg-secondary">POI</span> Canyon -->')
         .css("transform", `translateY(${screenPos(100)}px)`))
+
+    writeCampsites();
   
 });
